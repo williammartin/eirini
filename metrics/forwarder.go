5856a9979b7ff84359666b4e30a13a63504174d2
Mario Nitchev <marionitchev@gmail.com>
2018-11-19 11:43:45 +0200

    Add app info to container metrics. Change units

--- metrics/forwarder.go
+++ metrics/forwarder.go
@@ -17,10 +17,11 @@ func NewLoggregatorForwarder(client *loggregator.IngressClient) *LoggregatorForw
 func (l *LoggregatorForwarder) Forward(msg Message) {
 	l.client.EmitGauge(
 		loggregator.WithGaugeSourceInfo(msg.AppID, msg.IndexID),
-		loggregator.WithGaugeValue("cpu", msg.CPU, "%"),
-		loggregator.WithGaugeValue("memory", msg.Memory, msg.MemoryUnit),
-		loggregator.WithGaugeValue("disk", msg.Disk, msg.DiskUnit),
-		loggregator.WithGaugeValue("memory_quota", msg.MemoryQuota, msg.MemoryUnit),
-		loggregator.WithGaugeValue("disk_quota", msg.DiskQuota, msg.DiskUnit),
+		loggregator.WithAppInfo(msg.AppID, msg.IndexID),
+		loggregator.WithGaugeValue("cpu", msg.CPU, "percentage"),
+		loggregator.WithGaugeValue("memory", msg.Memory, "bytes"),
+		loggregator.WithGaugeValue("disk", msg.Disk, "bytes"),
+		loggregator.WithGaugeValue("memory_quota", msg.MemoryQuota, "bytes"),
+		loggregator.WithGaugeValue("disk_quota", msg.DiskQuota, "bytes"),
 	)
 }
