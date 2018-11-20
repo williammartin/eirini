e2ef70e353e0ced83f025a98ce2a107be571d1ff
Mario Nitchev <marionitchev@gmail.com>
2018-11-20 16:15:28 +0200

    Refactor unit conversion. Remove units from metrics message
    [#161542507]
    
    Signed-off-by: Georgi Dankov <gddankov@gmail.com>

--- metrics/forwarder.go
+++ metrics/forwarder.go
@@ -4,6 +4,12 @@ import (
 	loggregator "code.cloudfoundry.org/go-loggregator"
 )
 
+const (
+	cpuUnit    = "percentage"
+	memoryUnit = "bytes"
+	diskUnit   = "bytes"
+)
+
 type LoggregatorForwarder struct {
 	client *loggregator.IngressClient
 }
@@ -17,22 +23,10 @@ func NewLoggregatorForwarder(client *loggregator.IngressClient) *LoggregatorForw
 func (l *LoggregatorForwarder) Forward(msg Message) {
 	l.client.EmitGauge(
 		loggregator.WithGaugeSourceInfo(msg.AppID, msg.IndexID),
-		loggregator.WithGaugeValue("cpu", convertCPU(msg.CPU), "percentage"),
-		loggregator.WithGaugeValue("memory", convertMemory(msg.Memory), "bytes"),
-		loggregator.WithGaugeValue("disk", msg.Disk, "bytes"),
-		loggregator.WithGaugeValue("memory_quota", msg.MemoryQuota, "bytes"),
-		loggregator.WithGaugeValue("disk_quota", msg.DiskQuota, "bytes"),
+		loggregator.WithGaugeValue("cpu", msg.CPU, cpuUnit),
+		loggregator.WithGaugeValue("memory", msg.Memory, memoryUnit),
+		loggregator.WithGaugeValue("disk", msg.Disk, diskUnit),
+		loggregator.WithGaugeValue("memory_quota", msg.MemoryQuota, memoryUnit),
+		loggregator.WithGaugeValue("disk_quota", msg.DiskQuota, diskUnit),
 	)
-}
-
-// The kubernetes metrics api returns the cpu usage in "millicores"
-// which is described here: https://kubernetes.io/docs/concepts/configuration/manage-compute-resources-container/#meaning-of-cpu
-// This means that one millicore - 1m is equal to 0.1% of a CPU
-func convertCPU(cpuUsage float64) float64 {
-	return cpuUsage / 1000
-}
-
-// We assume that the kubernetes metrics api returns memory in Ki.
-func convertMemory(memoryUsage float64) float64 {
-	return memoryUsage * 1024
 }
